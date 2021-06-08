cfc4fdc9c43fe7ec98a5e8a9bf564ca74c10bb15
thomas <tkirkwood@spartaglobal.com>
2021-06-08 12:27:52 +0100

    updating to draft finished code

--- src/main/java/com/sparta/SimulationProject/Simulation.java
+++ src/main/java/com/sparta/SimulationProject/Simulation.java
@@ -5,13 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Simulation {
-    private int numberOfTrainingCentres;
-    private int numberOfTrainees;
+    private int numberOfTrainingCentres; // not neccesarry unless other code accesses it.
+    private int numberOfTraineesTotal;
+    private int numberOfTraineesCurrentlyTraining;
     private int numberOfTraineesOnWaitingList;
-    private List<TrainingCentre> trainingCentreList = new ArrayList<>();
+    private int numberOfFullTrainingcentres;
+    private List<TrainingCentre> trainingCentreList = new ArrayList<>();//can use list.size() for number of open centres
     private final int maxCapacity = 100;
 
-    public HashMap<Integer, int[]> runSimulation(int lengthOfTime){
+    public HashMap<Integer, int[]> runSimulation(int lengthOfTime) {
         HashMap<Integer, int[]> result = new HashMap<>();
 
 //        First training centre created at month 1
@@ -22,27 +24,51 @@ public class Simulation {
         numberOfTraineesOnWaitingList += RandomClass.newTrainees();
 
         for (int i = 1; i < lengthOfTime; i++) {
-            numberOfTraineesOnWaitingList += RandomClass.newTrainees();
-            if (i % 2 == 1){
+
+            // wipes every iteration to start fresh
+            int[] monthlyResult = new int[];
+
+            // numberOfTraineesOnWaitingList +=  RandomClass.newTrainees();
+            int tempNewTrainees = RandomClass.newTrainees();
+            numberOfTraineesTotal += tempNewTrainees;
+            numberOfTraineesOnWaitingList += tempNewTrainees;
+
+            if (i % 2 == 1) {
                 numberOfTrainingCentres++;
                 trainingCentreList.add(new TrainingCentre());
             }
-            for (TrainingCentre trainingCentre: trainingCentreList){
-                if (!trainingCentre.isFull()){
+            for (TrainingCentre trainingCentre : trainingCentreList) {
+                if (!trainingCentre.isFull()) {
                     int newTraineeIntake = trainingCentre.newTraineeIntake();
                     int traineesInCentre = trainingCentre.getTrainees();
                     int freeSpace = maxCapacity - traineesInCentre;
-                    if (freeSpace < newTraineeIntake){
+                    if (freeSpace < newTraineeIntake) {
                         trainingCentre.addTrainees(freeSpace);
                         numberOfTraineesOnWaitingList -= freeSpace;
-                    }else{
+                        //method name needs replaceing to correct method name
+                        trainingCentre.setIsFull(true);
+                        numberOfFullTrainingcentres++;
+
+                    } else {
                         trainingCentre.addTrainees(newTraineeIntake);
                         numberOfTraineesOnWaitingList -= newTraineeIntake;
                     }
                 }
             }
+            numberOfTraineesCurrentlyTraining = numberOfTraineesTotal - numberOfTraineesOnWaitingList;
+            // results to be displayed are num open centers, num full centers, num trainees currently training, num trainees on waiting list
+            monthlyResult[0] = numberOfTrainingCentres;
+            monthlyResult[1] = numberOfFullTrainingcentres;
+            monthlyResult[2] = numberOfTraineesCurrentlyTraining;
+            monthlyResult[3] = numberOfTraineesOnWaitingList;
+
+
+            result.put(i, monthlyResult)
+
         }
 
         return result;
     }
+
+
 }
