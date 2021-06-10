70486e6bc261c2fa5f8167ee17dccf02396d8ae4
AlexanderLegon <alexlegon2014@gmail.com>
2021-06-10 11:10:02 +0100

    changes to printer and finaldata

--- src/main/java/com/sparta/SimulationProject/FinalData.java
+++ src/main/java/com/sparta/SimulationProject/FinalData.java
@@ -14,15 +14,26 @@ public class FinalData {
     private static HashMap<Integer, int[]> traineesOnWaitingList = new HashMap<>();
 
     public static HashMap<Integer, int[]> getClosedCentres() {
+
         return closedCentres;
     }
+    public static int[] getClosedCentres(int i) {
 
+        int[] dataForMonth = closedCentres.get(i);
+        return dataForMonth;
+    }
     public static void addToClosedCentres(HashMap<Integer, int[]> closedCentresInput) {
         closedCentres.putAll(closedCentresInput);
     }
 
     public static HashMap<Integer, int[]> getOpenCentres() {
+
         return openCentres;
+    }
+    public static int[] getOpenCentres(int i) {
+
+        int[] dataForMonth = openCentres.get(i);
+        return dataForMonth;
     }
 
     public static void addToOpenCentres(HashMap<Integer, int[]> openCentresInput) {
@@ -30,7 +41,13 @@ public class FinalData {
     }
 
     public static HashMap<Integer, int[]> getFullCentres() {
+
         return fullCentres;
+    }
+    public static int[] getFullCentres(int i) {
+
+        int[] dataForMonth = fullCentres.get(i);
+        return dataForMonth;
     }
 
     public static void addToFullCentres(HashMap<Integer, int[]> fullCentresInput) {
@@ -38,7 +55,13 @@ public class FinalData {
     }
 
     public static HashMap<Integer, int[]> getTraineesTraining() {
+
         return traineesTraining;
+    }
+    public static int[] getTraineesTraining(int i) {
+
+        int[] dataForMonth = traineesTraining.get(i);
+        return dataForMonth;
     }
 
     public static void addToTraineesTraining(HashMap<Integer, int[]> traineesTrainingInput) {
@@ -47,6 +70,10 @@ public class FinalData {
 
     public static HashMap<Integer, int[]> getTraineesOnWaitingList() {
         return traineesOnWaitingList;
+    }
+    public static int[] getTraineesOnWaitingList(int i) {
+        int[] dataForMonth = traineesOnWaitingList.get(i);
+        return dataForMonth;
     }
 
     public static void addToTraineesOnWaitingList(HashMap<Integer, int[]> traineesOnWaitingListInput) {
